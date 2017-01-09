078ea1be26b38c4f6c259cc2e21eafbff390d181
bettinamaria <bettina_offenegger@hotmail.com>
2017-01-09 12:41:50 +0100

    Checkpoint 7 servicesPart2: completed

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,10 @@
  (function() {
-     function AlbumCtrl(Fixtures, $scope) {
+     function AlbumCtrl(Fixtures, $scope, SongPlayer) {
         $scope.albumData = Fixtures.getAlbum();
+        $scope.songPlayer = SongPlayer;
      }
  
      angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures','$scope', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures','$scope', AlbumCtrl, 'SongPlayer']);
  })();
